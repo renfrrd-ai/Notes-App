6543a353a4230eed5b0246ce6939e29df634f86b
Renfred <alongerenfred@gmail.com>
2026-03-24 07:49:09 -0400

    feat(api): add delete api endpoint

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const pg = require("pg");
 require("dotenv").config();
 
-const pool = new pg.Pool({
-  user: process.env.DB_USER_NAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const pool = require("../db");
+const { resolveSoa } = require("node:dns");
 
 router.get("/notes", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM notes");
     res.status(200).json(result.rows);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
@@ -37,8 +31,8 @@ router.post("/notes", async (req, res) => {
       [title, content],
     );
     res.status(201).json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
@@ -50,12 +44,41 @@ router.get("/notes/:id", async (req, res) => {
   }
 
   try {
-    const result = await pool.query(`SELECT * FROM notes WHERE id=$1`, [id]);
+    const result = await pool.query(
+      `SELECT * FROM notes
+       WHERE id=$1`,
+      [id],
+    );
+
     if (result.rows.length === 0)
       return res.status(404).json({ message: "Note not found" });
     res.status(200).json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/notes/:id", async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid ID" });
+  }
+
+  try {
+    const result = await pool.query(
+      `DELETE FROM notes
+       WHERE id=$1
+       RETURNING *`,
+      [id],
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
