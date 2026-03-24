65e3c6d6ab0573035ed5c28c41533abb0cd411c9
Renfred <alongerenfred@gmail.com>
2026-03-24 08:15:14 -0400

    feat(api): add put note endpoint

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -57,6 +57,38 @@ router.get("/notes/:id", async (req, res) => {
   }
 });
 
+router.put("/notes/:id", async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid ID" });
+  }
+
+  try {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res.status(400).json({
+        error: "Title and content are required",
+      });
+    }
+
+    const result = await pool.query(
+      `UPDATE notes
+       SET title = $1, content = $2
+       WHERE id=$3
+       RETURNING *`,
+      [title, content, id],
+    );
+
+    if (result.rows.length === 0)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 router.delete("/notes/:id", async (req, res) => {
   const id = parseInt(req.params.id, 10);
 
