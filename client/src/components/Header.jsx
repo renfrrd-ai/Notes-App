49e20902ed55a975bfce5d7ecd9d2c12cbb29c49
Renfred <alongerenfred@gmail.com>
2026-03-24 21:38:31 -0400

    feat: add Header Component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h2 className="text-4xl font-semibold mb-3">My Notes</h2>
+      <p className="text-sm text-gray-400">
+        Backend: Express + Postgres | Frontend: React + Tailwind
+      </p>
+      <hr className="border-gray-300 my-5 opacity-25" />
+    </header>
+  );
+}
+
+export default Header;
