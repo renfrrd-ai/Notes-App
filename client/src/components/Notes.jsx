82c1a486e67885e2063b55e7a758ece27f502ecf
Renfred <alongerenfred@gmail.com>
2026-03-25 12:19:44 -0400

    fix: display correct number of notes

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -9,7 +9,7 @@ function Notes({ notes, updateNotes }) {
       <div className="grid grid-cols-[6fr_1fr]">
         <h3 className="text-base font-semibold">Recent Notes</h3>
         <span className="max-w-20 text-xs text-gray-400 bg-black rounded-full flex text-center items-center justify-center">
-          3 Notes
+          {notes.length} Notes
         </span>
       </div>
       {notes
