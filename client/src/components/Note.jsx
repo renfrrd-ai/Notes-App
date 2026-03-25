c796371da3e2b45949ae817e6d37153d05c02aa2
Renfred <alongerenfred@gmail.com>
2026-03-25 12:17:52 -0400

    feat: add edit feature and connecting to backend(put request)

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -1,6 +1,26 @@
 import axios from "axios";
+import { useState, useRef } from "react";
 
 function Note({ id, title, content, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [note, setNote] = useState({ title, content });
+
+  async function handleInput(e) {
+    const attr = e.target.getAttribute("name");
+    const value = e.target.textContent.trim();
+    setNote((prev) => ({ ...prev, [attr]: value }));
+    setIsEditing(true);
+  }
+
+  async function handleSave() {
+    try {
+      await axios.put(`/notes/${id}`, note);
+      setIsEditing(false);
+    } catch (err) {
+      console.error("Save failed", err);
+    }
+  }
+
   async function deleteNote() {
     try {
       await axios.delete(`/notes/${id}`);
@@ -12,8 +32,24 @@ function Note({ id, title, content, onDelete }) {
   return (
     <div className="my-4 border-[.5px] border-neutral-600 rounded-lg p-4 grid grid-cols-[5fr_1fr]">
       <div>
-        <h4 className="text-md font-semibold block mb-1">{title}</h4>
-        <p className="text-sm text-gray-400">{content}</p>
+        <h4
+          className="text-md font-semibold block mb-1"
+          contentEditable="true"
+          onInput={handleInput}
+          name="title"
+          suppressContentEditableWarning={true}
+        >
+          {title}
+        </h4>
+        <p
+          className="text-sm text-gray-400 whitespace-pre-wrap"
+          contentEditable="true"
+          onInput={handleInput}
+          name="content"
+          suppressContentEditableWarning={true}
+        >
+          {content}
+        </p>
       </div>
       <div className="flex justify-between flex-col">
         <button
@@ -21,10 +57,15 @@ function Note({ id, title, content, onDelete }) {
           onClick={deleteNote}
         >
           Delete
-        </button>
-        <button className="invisible text-cyan-300 text-sm font-medium">
-          Save
-        </button>
+        </button>{" "}
+        {isEditing && (
+          <button
+            className="text-cyan-300 text-sm font-medium"
+            onClick={handleSave}
+          >
+            Save
+          </button>
+        )}
       </div>
     </div>
   );
