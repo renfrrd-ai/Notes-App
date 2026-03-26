c7af59d20a33eecabc5559818d4feccd03fda644
Renfred <alongerenfred@gmail.com>
2026-03-25 22:16:43 -0400

    feat: implement login feature using bcrypt and jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const pool = require("../db");
 const router = express.Router();
 
+const SECRET = process.env.JWT_SECRET;
 const saltRounds = 10;
 
 router.post("/register", async (req, res) => {
@@ -50,4 +53,47 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 });
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const result = await pool.query(
+      `SELECT * FROM users
+       WHERE email=$1
+       `,
+      [email],
+    );
+    if (result.rows.length == 0) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+    const user = result.rows[0];
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user.id, username: user.username }, SECRET, {
+      expiresIn: "1h",
+    });
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 3600000,
+      })
+      .json({
+        message: "Login successful",
+        user: { id: user.id, username: user.username, email: user.email },
+      });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
 module.exports = router;
