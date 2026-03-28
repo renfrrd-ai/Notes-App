3679e52ae1c938436d7da8c19fdaf19bedf11a5a
Renfred <alongerenfred@gmail.com>
2026-03-27 20:48:16 -0400

    style: add logo to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,12 @@
 function Header() {
   return (
     <header>
-      <h2 className="text-4xl font-semibold mb-3">My Notes</h2>
+      <div className="flex flex-start gap-3 mb-2">
+        <span className="text-3xl font-bold bg-blue-500 p-1 flex size-12 justify-center align-middle rounded-lg">
+          N
+        </span>
+        <h2 className="text-4xl font-semibold mb-3">My Notes</h2>
+      </div>
       <p className="text-sm text-gray-400">
         Backend: Express + Postgres | Frontend: React + Tailwind
       </p>
