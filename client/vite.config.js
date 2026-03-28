b74f49f399a5e56dcbf8a16c5b209086a8e173ef
Renfred <alongerenfred@gmail.com>
2026-03-27 22:02:21 -0400

    fix: api endpoint bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5040",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
